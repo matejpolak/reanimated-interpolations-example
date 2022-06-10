4d53544692bcada1c8b399e5d6ef4e94cc96aacc
Matěj Polák <matej.polaak@gmail.com>
2022-06-11 00:33:52 +0200

    feat: add sticky bottom with interpolation logic

---
+++ src/screens/StickyBottom/components/StickyItem/index.styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 200,
+    backgroundColor: '#8080f3',
+    marginVertical: 8,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 2
+  },
+  text: {
+    color: 'white',
+  },
+})
